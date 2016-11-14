063eb4f40964b866cf0b9889c006ed7203889099
Ibrahim <15390@IBRAHIMRAMOS>
2016-11-14 14:13:21 +0800

    Test commit

---
+++ ezcca/ezcca/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ezcca.Startup))]
+namespace ezcca
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
